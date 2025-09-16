40ae166c91bba8862a6a39dc20f53a5021814fea
youssouf TIMITE <youssouf.timite@edu.esiee.fr>
2025-09-16 08:54:40 +0000

    Implémentation de la fonction isprime pour vérifier les nombres premiers

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ from math import sqrt
 
 def isprime(p):
 
-    # votre code ici
-
-    pass
+    if p < 2:
+        return False
+    if p == 2:
+        return True
+    if p % 2 == 0:
+        return False
+    for i in range(3, int(sqrt(p)) + 1, 2):
+        if p % i == 0:
+            return False
+    return True
 
 #### Fonction principale
 
